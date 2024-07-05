c3f8daa7e43cc470875bbf647a123e243394ed7c
D Tim Cummings <tim@triptera.com.au>
2024-07-05 19:30:51 +1000

    show real data in generated web pages

--- django_bpaml_event/views.py
+++ django_bpaml_event/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+import datetime
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from django_bpaml_event.models import Event
 
 
 def index_page(request):
-    return render(request, 'django_bpaml_event/index.html', {})
+    """Find all events with an event date later than yesterday"""
+    yesterday = timezone.now() - datetime.timedelta(days=1)
+    list_future_events = Event.objects.filter(date__gte=yesterday).order_by("code")
+    context = {'list_events': list_future_events}
+    return render(request, 'django_bpaml_event/index.html', context)
 
 
 def event_page(request, code):
-    return render(request, 'django_bpaml_event/event.html', {'code': code})
+    e = get_object_or_404(Event, code=code)
+    context = {'event': e}
+    return render(request, 'django_bpaml_event/event.html', context)
