37e1d605c8f94986d6c24f113409f5fc7dd6dc36
D Tim Cummings <tim@triptera.com.au>
2024-07-05 19:40:13 +1000

    ability to change attendee status

--- django_bpaml_event/urls.py
+++ django_bpaml_event/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from django_bpaml_event.views import index_page, event_page
+from django_bpaml_event.views import index_page, event_page, attend
 
 urlpatterns = [
   path('', index_page, name='index'),
   path('event/<str:code>/', event_page, name='event'),
+  path('event/attend/<str:code>/<str:rsvp>/', attend, name='attend'),
 ]
--- django_bpaml_event/views.py
+++ django_bpaml_event/views.py
@@ -1,6 +1,8 @@
 import datetime
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+from django.http import HttpResponseRedirect
+from allauth.account.decorators import login_required
 from django_bpaml_event.models import Event
 
 
@@ -16,3 +18,25 @@ def event_page(request, code):
     e = get_object_or_404(Event, code=code)
     context = {'event': e}
     return render(request, 'django_bpaml_event/event.html', context)
+
+
+@login_required()
+def attend(request, code, rsvp):
+    """Set registration status for logged-in user as not attending
+    code: event code
+    rsvp: yes or no or true or false"""
+    user = request.user
+    e = get_object_or_404(Event, code=code)
+    # Add to event on y, yes, Y, Yes, t, True, true
+    if rsvp is None or len(rsvp) < 1:
+        pass  # do nothing
+    if rsvp.lower()[:1] in 'yt':
+        user.events.add(e)
+    elif rsvp.lower()[:1] in 'nf':
+        user.events.remove(e)
+    url_next = request.GET.get('next')
+    if url_next is None:
+        context = {'event': e}
+        return render(request, 'django_bpaml_event/event.html', context)
+    else:
+        return HttpResponseRedirect(url_next)
